69e5d3ef2f976f730df77e7ea2fce95269d25f3d
Aleksao998 <aleksaopacic988@gmail.com>
2024-09-03 19:08:06 +0200

    release v0.13.0

--- tests/ignition/modules/Incrementer.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-module.exports = buildModule("IncrementerModule", (m: any) => {
-  const incrementer = m.contract("Incrementer");
-
-  return { incrementer };
-});
